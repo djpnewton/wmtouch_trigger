a3dc08ce2b19a41388ef7476a3ffcc47a695474b
Daniel Newton <djpnewton@gmail.com>
2013-03-15 14:53:52 +1300

    initial code commit

---
+++ wmtouch_trigger/MainForm.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace wmtouch_trigger
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnActivate = new System.Windows.Forms.Button();
+            this.cbActivateOnce = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "WMTOUCH command:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(15, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(255, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btnActivate
+            // 
+            this.btnActivate.Location = new System.Drawing.Point(12, 57);
+            this.btnActivate.Name = "btnActivate";
+            this.btnActivate.Size = new System.Drawing.Size(75, 27);
+            this.btnActivate.TabIndex = 2;
+            this.btnActivate.Text = "Activate Command";
+            this.btnActivate.UseVisualStyleBackColor = true;
+            this.btnActivate.Click += new System.EventHandler(this.btnActivate_Click);
+            // 
+            // cbActivateOnce
+            // 
+            this.cbActivateOnce.AutoSize = true;
+            this.cbActivateOnce.Checked = true;
+            this.cbActivateOnce.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbActivateOnce.Location = new System.Drawing.Point(12, 90);
+            this.cbActivateOnce.Name = "cbActivateOnce";
+            this.cbActivateOnce.Size = new System.Drawing.Size(223, 21);
+            this.cbActivateOnce.TabIndex = 3;
+            this.cbActivateOnce.Text = "Only activate once per resume";
+            this.cbActivateOnce.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 255);
+            this.Controls.Add(this.cbActivateOnce);
+            this.Controls.Add(this.btnActivate);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.Text = "wmtouch trigger";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnActivate;
+        private System.Windows.Forms.CheckBox cbActivateOnce;
+    }
+}
+
---
+++ wmtouch_trigger/MainForm.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using Windows7.Multitouch;
+using Windows7.Multitouch.WinForms;
+
+namespace wmtouch_trigger
+{
+    public partial class MainForm : Form
+    {
+        int activateCount = 0;
+        TouchHandler touchHandler;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // setup power events handling
+            SystemEvents.PowerModeChanged += new PowerModeChangedEventHandler(SystemEvents_PowerModeChanged);
+            // setup touch events handling
+            touchHandler = Windows7.Multitouch.WinForms.Factory.CreateHandler<Windows7.Multitouch.TouchHandler>(this);
+            touchHandler.TouchDown += new EventHandler<TouchEventArgs>(touchHandler_TouchDown);
+            touchHandler.TouchMove += new EventHandler<TouchEventArgs>(touchHandler_TouchMove);
+            touchHandler.TouchUp += new EventHandler<TouchEventArgs>(touchHandler_TouchUp);
+        }
+
+        void SystemEvents_PowerModeChanged(object sender, PowerModeChangedEventArgs e)
+        {
+            switch (e.Mode)
+            {
+                case PowerModes.Suspend:
+                    break;
+                case PowerModes.Resume:
+                    activateCount = 0;
+                    break;
+            }
+        }
+
+        void touchHandler_TouchDown(object sender, Windows7.Multitouch.TouchEventArgs e)
+        {
+            ActivateCommand();
+        }
+
+        void touchHandler_TouchMove(object sender, Windows7.Multitouch.TouchEventArgs e)
+        {
+        }
+
+        void touchHandler_TouchUp(object sender, Windows7.Multitouch.TouchEventArgs e)
+        {
+        }
+
+        private void btnActivate_Click(object sender, EventArgs e)
+        {
+            ActivateCommand();
+        }
+
+        private void ActivateCommand()
+        {
+            if (cbActivateOnce.Checked && activateCount > 0)
+                return;
+            activateCount++;
+            try
+            {
+                System.Diagnostics.Process.Start(textBox1.Text);
+            }
+            catch (Exception e)
+            {
+            }
+        }
+    }
+}
